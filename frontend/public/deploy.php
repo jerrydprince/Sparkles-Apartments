4e9170c407c2270ea759cb7c8c1247e9bf299442
jerrydprince <jerrydprince@gmail.com>
2026-06-13 23:57:38 +0100

    deploy.php

--- frontend/public/deploy.php
+++ frontend/public/deploy.php
@@ -19,7 +19,7 @@ echo "Starting Git Deployment...\n";
 // Execute git commands to pull latest changes from GitHub
 // We run reset --hard to discard any local permission drift on the host
 // Discover repository path dynamically
-$repoPath = '/home/sparkle7/repositories/Sparkles-Hotel-Booking';
+$repoPath = '/home/sparkle7/public_html/gittest.sparklesapartments.ng/';
 if (!file_exists($repoPath)) {
     $repoPath = '/home/sparkle7/Sparkles-Hotel-Booking';
 }
