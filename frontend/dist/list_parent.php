6130e3dc359d45fe6a05154baff4d7406a7059a6
jerrydprince <jerrydprince@gmail.com>
2026-06-14 13:54:57 +0100

    diagnostics: search backend folder

--- frontend/dist/list_parent.php
+++ frontend/dist/list_parent.php
@@ -1,24 +1,31 @@
 <?php
 header("Content-Type: text/plain");
-echo "Listing for /home/sparkle7/test.sparklesapartments.ng/:\n";
+echo "Listing for /home/sparkle7/backend/:\n";
 
-$dir = '/home/sparkle7/test.sparklesapartments.ng/';
-if (is_dir($dir)) {
-    $files = scandir($dir);
-    foreach ($files as $file) {
-        $fullPath = $dir . $file;
-        if (is_file($fullPath)) {
-            $owner = function_exists('posix_getpwuid') ? posix_getpwuid(fileowner($fullPath))['name'] : fileowner($fullPath);
-            echo "  $file (" . filesize($fullPath) . " bytes) - Owner: $owner\n";
-            if ($file === 'clean.php') {
-                echo "--- CONTENTS OF clean.php ---\n";
-                echo file_get_contents($fullPath) . "\n";
-                echo "-----------------------------\n";
+$dirs = [
+    '/home/sparkle7/backend/',
+    '/home/sparkle7/backendgit/'
+];
+
+foreach ($dirs as $dir) {
+    echo "\nListing: $dir\n";
+    if (is_dir($dir)) {
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            $fullPath = $dir . $file;
+            if (is_file($fullPath)) {
+                $owner = function_exists('posix_getpwuid') ? posix_getpwuid(fileowner($fullPath))['name'] : fileowner($fullPath);
+                echo "  $file (" . filesize($fullPath) . " bytes) - Owner: $owner\n";
+                if ($file === 'clean.php') {
+                    echo "--- CONTENTS OF clean.php ---\n";
+                    echo file_get_contents($fullPath) . "\n";
+                    echo "-----------------------------\n";
+                }
+            } else {
+                echo "  $file/ [DIR]\n";
             }
-        } else {
-            echo "  $file/ [DIR]\n";
         }
+    } else {
+        echo "Directory does not exist.\n";
     }
-} else {
-    echo "Directory does not exist.\n";
 }
